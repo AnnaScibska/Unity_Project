0c24730705c07065f1607370afbc0714e684f3ee
Anna <anna.scibska@campusid.eu>
2019-12-08 20:21:11 +0100

    wall of fame scene added

---
+++ Assets/ResetHighscore.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetHighscore : MonoBehaviour
+{
+
+    public Transform entryContainer;
+    public Transform entryTemplate;
+
+    private List<HighscoreEntry> highscoreEntryList;
+    private List<Transform> highscoreEntryTransformList;
+
+    // SCRIPT TO RESET THE HIGHSCORE - DEV ENV ONLY
+    private void Awake()
+    {
+        entryTemplate.gameObject.SetActive(false);
+
+        // DATA TO PUT ON WALL OF FAME
+        highscoreEntryList = new List<HighscoreEntry>()
+        {
+            new HighscoreEntry{ score = 1000, name = "ANNA"},
+            new HighscoreEntry{ score = 999, name = "NICO"}
+        };
+
+        // Sort entry list by Score
+        for (int i = 0; i < highscoreEntryList.Count; i++)
+        {
+            for (int j = i + 1; j < highscoreEntryList.Count; j++)
+            {
+                if (highscoreEntryList[j].score > highscoreEntryList[i].score)
+                {
+                    // Swap
+                    HighscoreEntry tmp = highscoreEntryList[i];
+                    highscoreEntryList[i] = highscoreEntryList[j];
+                    highscoreEntryList[j] = tmp;
+                }
+            }
+        }
+
+        highscoreEntryTransformList = new List<Transform>();
+        foreach (HighscoreEntry highscoreEntry in highscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
+
+
+        Highscores highscores = new Highscores { highscoreEntryList = highscoreEntryList };
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+
+    }
+
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+    {
+        float templateHeight = 30f;
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        int rank = transformList.Count + 1;
+
+        // converting the rank
+        string rankString;
+
+        switch (rank)
+        {
+            case 1: rankString = "1ST"; break;
+            case 2: rankString = "2ND"; break;
+            case 3: rankString = "3RD"; break;
+            default: rankString = rank + "TH"; break;
+        }
+
+        entryTransform.Find("positonText").GetComponent<Text>().text = rankString;
+
+        int score = highscoreEntry.score;
+        entryTransform.Find("scoreText").GetComponent<Text>().text = score.ToString();
+
+        string name = highscoreEntry.name;
+        entryTransform.Find("nameText").GetComponent<Text>().text = name;
+
+        transformList.Add(entryTransform);
+
+    }
+
+    private class Highscores
+    {
+        public List<HighscoreEntry> highscoreEntryList;
+    }
+
+    /*
+     * Represents a single High score entry
+     * */
+    [System.Serializable] // for json // PlayerPrefs
+    private class HighscoreEntry
+    {
+        public int score;
+        public string name;
+    }
+}
+
