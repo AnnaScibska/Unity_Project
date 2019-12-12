89b4b21be85b973d4ce29f5d30ca19f449a62429
Anna <anna.scibska@campusid.eu>
2019-12-12 20:17:13 +0100

    game over scene recreated

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadLevel : MonoBehaviour {
 
 	public string levelname = "enter level name";
 	public void OnTriggerEnter2D (Collider2D other) {
 		if (other.tag == "Player") {
-			Application.LoadLevel (levelname);
+			SceneManager.LoadScene (levelname);
 		}
 	}
 }
