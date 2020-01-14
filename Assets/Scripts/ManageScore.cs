1ac84a67b18e4ad5bb8521d19bf14137ce3b87c2
npietri <nicolaspietri.fr@gmail.com>
2020-01-14 18:18:04 +0100

    new managedSoreWorking

--- Assets/Scripts/ManageScore.cs
+++ Assets/Scripts/ManageScore.cs
@@ -9,20 +9,20 @@ public class ManageScore : MonoBehaviour {
 
     Text text;
 
-    void Awake()
-    {
-        text = GetComponent<Text>();
+    void start () {
         score = 0;
+    }
+    void Awake () {
+        text = GetComponent<Text> ();
+        DontDestroyOnLoad (transform.gameObject);
     }
 
     // updating score on the screen with every enemy killed
-    void Update()
-    {
+    void Update () {
         text.text = "SCORE: " + score;
     }
 
-    public static int GetScore()
-    {
+    public static int GetScore () {
         return score;
     }
 
